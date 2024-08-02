38237b742d9f6d1b415654bb878c6c6f35efa1fc
YounChaeEun <amy4021186@gmail.com>
2024-08-02 19:18:16 +0900

    #94 feat: Redis 초기 설정 및 Redis 서비스 추가

--- src/main/java/backend/taskweaver/global/redis/RedisService.java
+++ src/main/java/backend/taskweaver/global/redis/RedisService.java
@@ -1,41 +1,41 @@
 package backend.taskweaver.global.redis;
 
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.data.redis.core.HashOperations;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.core.ValueOperations;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.time.Duration;
-//import java.util.Map;
-//import java.util.concurrent.TimeUnit;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class RedisService {
-//
-//    private final RedisTemplate<String, String> redisTemplate;
-//
-//    public void setValues(String key, String data) {
-//        ValueOperations<String, String> values = redisTemplate.opsForValue();
-//        values.set(key, data);
-//    }
-//
-//    @Transactional(readOnly = true)
-//    public String getValues(String key) {
-//        ValueOperations<String, String> values = redisTemplate.opsForValue();
-//        if (values.get(key) == null) {
-//            return "false";
-//        }
-//        return values.get(key);
-//    }
-//
-//    public void deleteValues(String key) {
-//        redisTemplate.delete(key);
-//    }
-//
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void setTokenValues(String key, UserTokens tokenValue) throws JsonProcessingException {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        String jsonValue = objectMapper.writeValueAsString(tokenValue);
+        values.set(key, jsonValue);
+    }
+
+    @Transactional(readOnly = true)
+    public UserTokens getTokenValues(String key) throws JsonProcessingException {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        String jsonValue = values.get(key);
+        if (jsonValue == null) {
+            return null;
+        }
+        return objectMapper.readValue(jsonValue, UserTokens.class);
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+
 //    public boolean checkExistsValue(String value) {
 //        return !value.equals("false");
 //    }
-//}
+}
